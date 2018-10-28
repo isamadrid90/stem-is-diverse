80bfff4ada0a0a91d1fd8fbc8a1c3d73210da35b
isabel garrido <isa.madrid90@gmail.com>
2018-10-28 17:58:19 +0100

    Updates

---
+++ precache-manifest.9f7e16e091338af254d4330e4fece5a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5f0a46d975378e61dc60",
+    "url": "/stem-is-diverse/static/css/main.78006b2e.chunk.css"
+  },
+  {
+    "revision": "5f0a46d975378e61dc60",
+    "url": "/stem-is-diverse/static/js/main.5f0a46d9.chunk.js"
+  },
+  {
+    "revision": "8f4f696b8a7f439d0273",
+    "url": "/stem-is-diverse/static/js/1.8f4f696b.chunk.js"
+  },
+  {
+    "revision": "9bc322c06455d1784f58",
+    "url": "/stem-is-diverse/static/js/runtime~main.9bc322c0.js"
+  },
+  {
+    "revision": "7404947ba96ab7505903d2c57b2e665f",
+    "url": "/stem-is-diverse/index.html"
+  }
+];
