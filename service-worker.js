e693544e4f30b09b1a37ef4f131b3274b2fe9536
isabel garrido <isa.madrid90@gmail.com>
2018-11-17 20:09:08 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/stem-is-diverse/precache-manifest.95c74934b98b786aa0f66ca99d18e2cf.js"
+  "/stem-is-diverse/precache-manifest.d4910cea1ad6c42c86a0d22928d14fd2.js"
 );
 
 workbox.clientsClaim();
