eb30498600b87d2e40f06d844d21fd9249f0d32a
isabel garrido <isa.madrid90@gmail.com>
2018-11-17 20:07:46 +0100

    Redesign

--- src/components/People.js
+++ src/components/People.js
@@ -1,26 +1,31 @@
 import React, { Component } from 'react';
 import Person from './Person';
-import './People.css';
-import peopleList from "../data.json"
+//import './People.css';
+import peopleList from "../data.json";
+import GridList from '@material-ui/core/GridList';
+
 
 class People extends Component {
   
   render() {
     let peopleListRaw = peopleList.map(person => {
       return (
-        <Person picture={person.picture}
-        altText={person.altText} 
-        name={person.name} 
-        country={person.country} 
-        tags={person.tags} 
-        history={person.history} 
-        dates={person.dates}
-        link={person.link}/>
+          <Person key={person.id}
+          picture={person.picture}
+          altText={person.altText} 
+          name={person.name} 
+          country={person.country} 
+          tags={person.tags} 
+          history={person.history} 
+          dates={person.dates}
+          link={person.link}/>
       );
     });
     return (
       <section className="People">
-      {peopleListRaw}
+      <GridList cellHeight={180}>
+        {peopleListRaw}
+      </GridList>
       </section>
     );
   }
--- src/components/Person.js
+++ src/components/Person.js
@@ -1,25 +1,45 @@
 import React, { Component } from 'react';
-import './Person.css';
+//import './Person.css';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+import IconButton from '@material-ui/core/IconButton';
 
 
 class Person extends Component {
   render() {
     return (
-      <div className="Person">
-        <div className="Picture">
-            <img src={this.props.picture} alt={this.props.altText}></img>
-        </div>
-        <div className="information">
-            <div className="Name"><span>{this.props.name}</span></div>
-            <div className="Header">
-                <span className="Country">{this.props.country}</span>
-                <div className="Dates">{this.props.dates}</div>
-                <div className="Tags">{this.props.tags}</div>
-            </div>
-            <div className="History">{this.props.history}</div>
-            <a target="_blank" rel="noopener noreferrer" className="More" href={this.props.link}>More details</a>
-        </div>
-      </div>
+      // <div key={this.props.id}>
+      // <Grid container spacing={16} className="Person">
+
+      //   <Grid item xs={4} sm={4} lg={5} xl={4} className="Picture" >
+      //         <img src={this.props.picture} alt={this.props.altText}></img>
+      //   </Grid>
+      //   <Grid item xs={8} sm={8} lg={7} xl={8}>
+      //     <div className="information">
+      //         <div className="Name"><span>{this.props.name}</span></div>
+      //         <div className="Header">
+      //             <span className="Country">{this.props.country}</span>
+      //             <div className="Dates">{this.props.dates}</div>
+      //             <div className="Tags">{this.props.tags}</div>
+      //         </div>
+      //         <div className="History">{this.props.history}</div>
+      //         <a target="_blank" rel="noopener noreferrer" className="More" href={this.props.link}>More details</a>
+      //     </div>
+      //     </Grid>
+      // </Grid>
+      // </div>
+
+      <GridListTile key={this.props.picture}>
+            <img src={this.props.picture} alt={this.props.altText} />
+            <GridListTileBar
+              title={this.props.name}
+              subtitle={<span>{this.props.tags}</span>}
+              actionIcon={
+                <IconButton >
+                </IconButton>
+              }
+            />
+        </GridListTile>
     );
   }
 }
